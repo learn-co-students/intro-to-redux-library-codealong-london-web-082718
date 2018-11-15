05eb9826c8fc390d0c316b2f151faee0c82f7467
braj011 <bhagyaraj01@gmail.com>
2018-11-15 16:52:56 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -5,6 +5,13 @@ import ReactDOM from 'react-dom';
 import { createStore } from 'redux'; /* code change */
 import shoppingListItemReducer from './reducers/shoppingListItemReducer.js';
 import App from './App';
+import { Provider } from 'react-redux'
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(shoppingListItemReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+
+ReactDOM.render(
+  <Provider store={store}>
+      <App />
+  </Provider> /* code change */, 
+  document.getElementById('root'));
