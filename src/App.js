02adced89dade4d93db71b20c65f716ee38760b5
klbennett <thestylishkidsintheriot@gmail.com>
2018-11-17 17:29:08 +0000

    Done.

--- src/App.js
+++ src/App.js
@@ -1,37 +1,37 @@
-    // ./src/App.js
-     
-    import React, { Component } from 'react';
-    import { connect } from 'react-redux';
-    import './App.css';
-     
-    class App extends Component {
-        handleOnClick = event => {
-            this.props.increaseCount();
-        };
-     
-        render() {
-            return (
-                <div className="App">
-                    <button onClick={this.handleOnClick}>Click</button>
-                    <p>{this.props.items.length}</p>
-                </div>
-            );
-        }
+// ./src/App.js
+
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import './App.css';
+
+class App extends Component {
+    handleOnClick = event => {
+        this.props.increaseCount();
+    };
+
+    render() {
+        return (
+            <div className="App">
+                <button onClick={this.handleOnClick}>Click</button>
+                <p>{this.props.items.length}</p>
+            </div>
+        );
     }
-     
-    const mapStateToProps = state => {
-        return {
-            items: state.items
-        };
+}
+
+const mapStateToProps = state => {
+    return {
+        items: state.items
     };
-     
-    const mapDispatchToProps = dispatch => {
-        return {
-            increaseCount: () => dispatch({ type: 'INCREASE_COUNT' })
-        };
+};
+
+const mapDispatchToProps = dispatch => {
+    return {
+        increaseCount: () => dispatch({ type: 'INCREASE_COUNT' })
     };
-     
-    export default connect(
-        mapStateToProps,
-        mapDispatchToProps
-    )(App);
+};
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(App);
